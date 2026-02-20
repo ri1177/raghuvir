6483abf9c7e778e92041ec15c686f465b52c0b8e
ri1177 <dangerishil28@gmail.com>
2026-02-20 10:26:11 +0530

    Change base path in Vite config from './' to '/'

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",
   build: {
     outDir: "dist",
     chunkSizeWarningLimit: 5000,
@@ -15,3 +15,4 @@ export default defineConfig({
     },
   },
 });
+
